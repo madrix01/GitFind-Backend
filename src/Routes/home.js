1d2b2d41d79d709ba970046260e505459f16ab93
madrix01 <shlokcpate2001@gmail.com>
2021-02-25 02:30:22 +0530

    + Adding repo to the feed

--- src/Routes/home.js
+++ src/Routes/home.js
@@ -20,7 +20,27 @@ router.get('/user', async (req, res) => {
 
 //Post a repo
 router.post('/postRepo', async (req, res) => {
-    console.log(req);
+    var resp = await axios.get(`https://api.github.com/repos/${storage.state.username}/${req.body.repoName}`);    
+    resp = resp.data;
+    const body = {
+        repoName : req.body.repoName,
+        link : `https://github.com/${storage.state.username}/${req.body.repoName}`,
+        tags : req.body.tags,
+        level : req.body.level,
+        readme : `https://github.com/${storage.state.username}/${req.body.repoName}/blob/main/readme.md`,
+        updated_at : resp.updated_at,
+        stars: resp.stargazers_count,
+        forks : resp.forks,
+        language: resp.language
+    }
+    await axios.get(`https://github.com/${storage.state.username}/${req.body.repoName}/blob/main/readme.md`).catch(err => body.readme = null);
+    // if(checkReadme.status === 404){
+    //     body.readme = null
+    // }
+    const repoRef = db.collection('repos').doc(req.body.repoName);
+
+    await repoRef.set(body);
+    console.log(body);
     res.send("Fuck");
 })
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const express= require("express");
 const dotenv = require("dotenv");
-
+const cors = require('cors');
 const app = express();
 // Routes
 const authRoute = require('./Routes/auth').router;
 const homeRoute = require('./Routes/home');
+
 dotenv.config();
 
 const clientID = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
 
+app.use(express.json());
+app.use(cors());
 
 app.use('/api', authRoute);
 app.use('/api/home', homeRoute);
-app.use(express.json());
   
 app.listen(6969, () => {
     console.log('Ayy Ayy captain');
